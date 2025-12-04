2ca0e0c82466c20ec9b8f059486c801c851a708e
Szymon-7 <szymonpatryn@gmail.com>
2025-12-03 19:10:44 -0500

    image to ascii art working somewhat - todo: scaling, maybe colours

--- src/main_image.c
+++ src/main_image.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         printf("Usage: %s <IMAGE_FILE>\n", argv[0]);
         return 1;
     }
 
-    char *file_path = argv[1];
-
-    FILE *fptr = fopen(file_path, "rb");
-    if (fptr == NULL) {
-        printf("File not found\n");
+    char *ASCII = "@%#*+=-:. ";
+    int width, height, channels;
+    unsigned char *img = stbi_load(argv[1], &width, &height, &channels, 1);
+    if (img == NULL) {
+        printf("Error: Image not found\n");
         return 1;
     }
 
-    fclose(fptr);
+    FILE *out = fopen("output.txt", "w");
+
+    for (int y = 0; y < height; y += 3) {
+        for (int x = 0; x < width; x++) {
+            unsigned char pixel = img[y * width + x];
+            int i = (pixel * 9) / 255;
+            fprintf(out, "%c", ASCII[i]);
+        }
+        fprintf(out, "\n");
+    }
+
+    printf("Generated: output.txt\n");
+
+    stbi_image_free(img);
+    fclose(out);
     return 0;
 }
