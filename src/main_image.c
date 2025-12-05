ab282be47a7e7e3d8b339b15a65da685d11f7a63
Szymon-7 <szymonpatryn@gmail.com>
2025-12-04 23:32:23 -0500

    scaling working - aspect ratio not perfect but about the best it can be - todo: colours next

--- src/main_image.c
+++ src/main_image.c
@@ -4,8 +4,14 @@
 #include "stb_image.h"
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("Usage: %s <IMAGE_FILE>\n", argv[0]);
+    if (argc < 3) {
+        printf("Usage: %s <IMAGE_FILE> <scale>\n", argv[0]);
+        return 1;
+    }
+
+    float scale = atof(argv[2]);
+    if (scale <= 0 || scale > 1) {
+        printf("Scale must be between 0 and 1.\n");
         return 1;
     }
 
@@ -17,18 +23,24 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    int scaled_w = width * scale;
+    int scaled_h = height * scale * 0.5;
+
     FILE *out = fopen("output.txt", "w");
 
-    for (int y = 0; y < height; y += 3) {
-        for (int x = 0; x < width; x++) {
-            unsigned char pixel = img[y * width + x];
+    for (int y = 0; y < scaled_h; y++) {
+        for (int x = 0; x < scaled_w; x++) {
+            int src_x = x / scale;
+            int src_y = y / (scale * 0.5);
+
+            unsigned char pixel = img[src_y * width + src_x];
             int i = (pixel * 9) / 255;
             fprintf(out, "%c", ASCII[i]);
         }
         fprintf(out, "\n");
     }
 
-    printf("Generated: output.txt\n");
+    printf("Generated: output.txt (%dx%d scaled to %dx%d)\n", width, height, scaled_w, scaled_h);
 
     stbi_image_free(img);
     fclose(out);
